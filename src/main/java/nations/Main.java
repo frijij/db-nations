cefb51f99f0a33b2be3e20eca6f17c87eb7b4c1c
francesca <francipeco36@gmail.com>
2023-09-07 13:54:44 +0200

    doing bonus: preparing languages list per country

--- src/main/java/nations/Main.java
+++ src/main/java/nations/Main.java
@@ -1,6 +1,7 @@
 package nations;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +14,12 @@ public class Main {
             "JOIN regions r ON c.region_id = r.region_id " +
             "JOIN continents c2 ON r.continent_id = c2.continent_id " +
             "WHERE c.name LIKE ? ORDER BY c.name;";
+    private final static String ID_QUERY= "SELECT *\n" +
+            "FROM countries c \n" +
+            "JOIN country_languages cl ON c.country_id = cl.country_id \n" +
+            "JOIN languages l ON cl.language_id = l.language_id \n" +
+            "JOIN country_stats cs ON c.country_id =cs.country_id \n" +
+            "WHERE c.country_id = ? ;";
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -20,7 +27,7 @@ public class Main {
         try(Connection connection = DriverManager.getConnection(URL, USER, PW)){
             System.out.println("Search... ");
             String search = "%"+scan.nextLine()+"%";
-            System.out.println("Searching results for '" + search + "' ...");
+            System.out.println("Results for '" + search + "' ...");
             try(PreparedStatement ps = connection.prepareStatement(QUERY)){
                ps.setString(1, search);
                 try(ResultSet rs = ps.executeQuery()){
@@ -33,10 +40,29 @@ public class Main {
                         }
                 }
             }
+            System.out.println("Enter country id to see details: ");
+            int choiceId = Integer.parseInt(scan.nextLine());
+            try (PreparedStatement ps = connection.prepareStatement(ID_QUERY)){
+                ps.setInt(1, choiceId);
+                try (ResultSet rs = ps.executeQuery()){
+                    rs.next();
+                        int countryId = rs.getInt("country_id");
+                        String countryName=rs.getString("name");
+                        System.out.println("Details for: " + countryName);
+                        System.out.println(countryId);
+                    System.out.print("Languages: ");
+                    ArrayList<String> languages = new ArrayList<String>();
 
+                        while (rs.next()){
+                            String language = rs.getString("language");
+                            languages.add(language);
+                        }
+                    System.out.println("Languages:" +languages);
+
+                }
+            }
         } catch (SQLException e){
             System.out.print("Connessione al database non riuscita");
-            e.printStackTrace();
         }
 
         scan.close();
