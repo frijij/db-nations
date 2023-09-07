c4874850032b636ab8b1cee7c72e73003a67fec1
francesca <francipeco36@gmail.com>
2023-09-07 12:32:03 +0200

    milestone 3 completed

--- src/main/java/nations/Main.java
+++ src/main/java/nations/Main.java
@@ -8,30 +8,35 @@ public class Main {
     private final static String URL = "jdbc:mysql://localhost:3306/db_nations";
     private final static String USER = "root";
     private final static String PW = "Java2023!";
-    private final static String QUERY = "SELECT c.name AS country_name, c.country_id, r.name AS region, c2.name AS continent \n" +
-        "FROM countries c\n" +
-        "JOIN regions r ON c.region_id = r.region_id \n" +
-        "JOIN continents c2 ON r.continent_id = c2.continent_id \n" +
-        "ORDER BY c.name;";
+    private final static String QUERY = "SELECT c.name AS country_name, c.country_id, r.name AS region, c2.name AS continent " +
+            "FROM countries c " +
+            "JOIN regions r ON c.region_id = r.region_id " +
+            "JOIN continents c2 ON r.continent_id = c2.continent_id " +
+            "WHERE c.name LIKE ? ORDER BY c.name;";
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         try(Connection connection = DriverManager.getConnection(URL, USER, PW)){
+            System.out.println("Search... ");
+            String search = "%"+scan.nextLine()+"%";
+            System.out.println("Searching results for '" + search + "' ...");
             try(PreparedStatement ps = connection.prepareStatement(QUERY)){
-                try(ResultSet rs = ps.executeQuery(QUERY)){
+               ps.setString(1, search);
+                try(ResultSet rs = ps.executeQuery()){
                     while(rs.next()){
-                        String countryName= rs.getString("country_name");
-                        int countryId = rs.getInt("country_id");
-                        String region=rs.getString("region");
-                        String continent=rs.getString("continent");
-                        System.out.println(countryName + " | " + countryId + " | " + region + " | " + continent);
-                    }
+                            String countryName= rs.getString("country_name");
+                            int countryId = rs.getInt("country_id");
+                            String region=rs.getString("region");
+                            String continent=rs.getString("continent");
+                            System.out.println(countryName + " | " + countryId + " | " + region + " | " + continent);
+                        }
                 }
             }
 
         } catch (SQLException e){
             System.out.print("Connessione al database non riuscita");
+            e.printStackTrace();
         }
 
         scan.close();
